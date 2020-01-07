17b3637aebe35cb3a6fe2c7d649d383c988457f0
wyattpease <wyattpease@gmail.com>
2020-01-06 18:35:50 -0600

    Initial

--- AudioGenre/AudioGenre.py
+++ AudioGenre/AudioGenre.py
@@ -1,9 +1,21 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.model_selection import KFold, cross_val_score
+
+#This code will predict whether a song will be listed as 'Rock' or 'Hip-Hop' 
+#depending its associated features, such as 'danceability', 'energy', or 'speechiness'
 
 # Read in track metadata with genre labels
 tracks = pd.read_csv('datasets/fma-rock-vs-hiphop.csv')
 
-# Read in track metrics with the features
+# Read in track metrics with the song features
 echonest_metrics = pd.read_json('datasets/echonest-metrics.json', precise_float=True)
 
 # Merge the relevant columns of tracks and echonest_metrics
@@ -12,26 +24,21 @@ print(echo_tracks.head())
 # Inspect the resultant dataframe
 echo_tracks.info()
 
-# Create a correlation matrix
+# Determine if any features have strong correlations in order to 
+# eliminate any potential feature redundancy
 corr_metrics = echo_tracks.corr()
 corr_metrics.style.background_gradient()
 
-# Define our features 
+# Define features and labels
 features = echo_tracks.drop(columns=['genre_top','track_id'], axis=1)
-
-# Define our labels
 labels = echo_tracks['genre_top']
 
-# Import the StandardScaler
-from sklearn.preprocessing import StandardScaler
-
-# Scale the features and set the values to a new variable
+# No string correlatoins can be seen, going to use PCA to 
+# reduce the number of components
+# First, going to normalize the data to avoid bias towards features with broader ranges
 scaler = StandardScaler()
 scaled_train_features = scaler.fit_transform(features)
 
-# Import our plotting module, and PCA class
-import matplotlib.pyplot as plt
-from sklearn.decomposition import PCA
 # Get our explained variance ratios from PCA using all features
 pca = PCA()
 pca.fit(scaled_train_features)
@@ -42,10 +49,8 @@ fig, ax = plt.subplots()
 ax.bar(range(pca.n_components_), exp_variance)
 ax.set_xlabel('Principal Component #')
 
-# Import numpy
-import numpy as np
-
-# Calculate the cumulative explained variance
+# No clear elbow in plot, going to use cumulative explained variance 
+# to determine how many features are required to explain 90% of the variance
 cum_exp_variance = np.cumsum(exp_variance)
 
 # Plot the cumulative explained variance and draw a dashed line at 0.90.
@@ -54,49 +59,40 @@ ax.plot(cum_exp_variance)
 ax.axhline(y=0.9, linestyle='--')
 n_components = 6
 
-# Perform PCA with the chosen number of components and project data onto components
+# Perform PCA with the chosen number of components and project data onto 
+# components in order to classify songs into genres
 pca = PCA(n_components, random_state=10)
 pca.fit(scaled_train_features)
 pca_projection = pca.transform(scaled_train_features)
 
-# Import train_test_split function and Decision tree classifier
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-
-# Split our data
+# Split data into test and train
 train_features, test_features, train_labels, test_labels = train_test_split(pca_projection, labels, random_state=10)
 
-# Train our decision tree
+# Train decision tree
 tree = DecisionTreeClassifier(random_state=10)
 tree.fit(train_features, train_labels)
 
 # Predict the labels for the test data
 pred_labels_tree = tree.predict(test_features)
 
-# Import LogisticRegression
-from sklearn.linear_model import LogisticRegression
-
-# Train our logistic regression and predict labels for the test set
+# Train the logistic regression and predict labels for the test set
 logreg = LogisticRegression(random_state=10)
 logreg.fit(train_features, train_labels)
 pred_labels_logit = logreg.predict(test_features)
 
-# Create the classification report for both models
-from sklearn.metrics import classification_report
+# Create the classification report for both logistic and tree models
+
 class_rep_tree = classification_report(test_labels, pred_labels_tree)
 class_rep_log = classification_report(test_labels, pred_labels_logit)
 
 print("Decision Tree: \n", class_rep_tree)
 print("Logistic Regression: \n", class_rep_log)
 
-# Subset only the hip-hop tracks, and then only the rock tracks
 hop_only = echo_tracks.loc[echo_tracks['genre_top'] == 'Hip-Hop']
 rock_only = echo_tracks.loc[echo_tracks['genre_top'] == 'Rock']
 
 # sample the rocks songs to be the same number as there are hip-hop songs
 rock_only = echo_tracks.loc[echo_tracks['genre_top'] == 'Rock'].sample(len(hop_only.index), random_state=10)
-
-# concatenate the dataframes rock_only and hop_only
 rock_hop_bal = pd.concat([rock_only, hop_only])
 
 # The features, labels, and pca projection are created for the balanced dataframe
@@ -107,12 +103,12 @@ pca_projection = pca.fit_transform(scaler.fit_transform(features))
 # Redefine the train and test set with the pca_projection from the balanced data
 train_features, test_features, train_labels, test_labels = train_test_split(pca_projection, labels, random_state=10)
 
-# Train our decision tree on the balanced data
+# Train the decision tree on the balanced data
 tree = DecisionTreeClassifier(random_state=10)
 tree.fit(train_features, train_labels)
 pred_labels_tree = tree.predict(test_features)
 
-# Train our logistic regression on the balanced data
+# Train the logistic regression on the balanced data
 logreg = LogisticRegression(random_state=10)
 logreg.fit(train_features, train_labels)
 pred_labels_logit = logreg.predict(test_features)
@@ -121,15 +117,12 @@ pred_labels_logit = logreg.predict(test_features)
 print("Decision Tree: \n", classification_report(test_labels, pred_labels_tree))
 print("Logistic Regression: \n", classification_report(test_labels, pred_labels_logit))
 
-from sklearn.model_selection import KFold, cross_val_score
-
-# Set up our K-fold cross-validation
+# Set up K-fold cross-validation
 kf = KFold(n_splits=10, random_state=10)
-
 tree = DecisionTreeClassifier(random_state=10)
 logreg = LogisticRegression(random_state=10)
 
-# Train our models using KFold cv
+# Train the models using KFold cv
 tree_score = cross_val_score(tree, pca_projection, labels, cv=kf)
 logit_score = cross_val_score(logreg, pca_projection, labels, cv=kf)
 
